6f496d5d4b3710664fe66ecb48c788e9038c3c7e
rahidaliyev <rahidaliyev18@gmail.com>
2022-05-14 21:27:22 +0400

    Change LIst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import CardForm from "./components/Form/Form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*
+По закрытию карты удаляйте ее из списка карт клиента
+*/
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { number: "7653 7553 5693 9862", balance: 100 },
+        { number: "7453 9736 0763 3474", balance: 400 },
+        { number: "9577 7543 9379 9784", balance: 800 }
+      ]
+    };
+  }
+  handleCloseCard = (idx) => {
+    const clone = [ ...this.state.cards ];
+    clone.splice(idx, 1);
+    this.setState({ cards: clone });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleOpenCard = (card) => {
+   
+    console.log(card);
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <CardForm handleOpenCard={this.handleOpenCard} />
+        {this.state.cards.map(({ number, balance }, idx) => (
+          <div key={number} className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">
+                Карта <br />
+                {number}
+              </h5>
+              <div className="card-text">
+                <ul className="list-group">
+                  <li className="list-group-item">Баланс: {balance}</li>
+                  <hr />
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => this.handleCloseCard(idx)}
+                  >
+                    Закрыть карту
+                  </button>
+                </ul>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
